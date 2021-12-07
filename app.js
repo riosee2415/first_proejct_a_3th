47ab0d579f7083211cb783f24115a169ce267d0d
4LEAF.YSH <4leaf.ysh@gmail.com>
2021-12-07 16:23:50 +0900

    conf

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const PORT = 4000;
+
+app.use(morgan("dev"));
 
 const test = [];
 
