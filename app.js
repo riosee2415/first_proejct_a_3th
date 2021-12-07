9f7654285893f8764901dae27e30dcb9f072355d
4LEAF-LSH <4leaf.lsh@gmail.com>
2021-12-07 16:25:42 +0900

    modiyl

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const PORT = 4000;
 
 const test = [];
 const test12 = {};
+
+const kig = 12313;
+const ab = "이름";
+const name1 = "kigkm";
+const name2 = "kigkm";
+const name3 = "kigkm";
+const name4 = "kigkm";
+const name5 = "kigkm";
 app.listen(PORT, () => {
   console.log(`${PORT} SERVER START`);
 });
